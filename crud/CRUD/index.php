1889a2c48e146e7c0e58570fee2b4fd2cc84babd
EstevaoDanielli <86986832+EstevaoDanielli@users.noreply.github.com>
2022-02-15 11:45:31 -0300

    Delete crud/CRUD directory

--- crud/CRUD/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require __DIR__.'/vendor/autoload.php';
-    use \App\Entity\Usuario;
-
-    $users = Usuario::getUsers();
-
-    include __DIR__.'/includes/header.php';
-    include __DIR__.'/includes/listaCadastros.php';
-    include __DIR__.'/includes/footer.php';
